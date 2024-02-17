b0981e6586d613674dd38d769cbd095a42a01530
hoppas0 <2622850487@qq.com>
2024-02-17 17:46:16 +0800

    update main.py

--- main.py
+++ main.py
@@ -235,6 +235,6 @@ class TypingGames:
         self.window.blit(self.scoreBoard, (15, WINDOW_Y_LENGTH - 40))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     tg = TypingGames()
     tg.run()
